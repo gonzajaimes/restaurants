99eb34233a57d4a00cde3a8cb6feb9a26e0cb0a8
Gonzalo Jaimes <gonzajaimes@hotmail.com>
2023-01-04 12:36:25 -0500

    iconos de barra

--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -8,13 +8,51 @@ import FavouritesStack from './FavouritesStack'
 import TopRestaurantsStack from './TopRestaurantsStack'
 import SearchStack from './SearchStack'
 import AccountStack from './AccountStack'
+import { Icon } from '@rneui/base'
 
 const Tab = createBottomTabNavigator()
 
 export default function Navigation() {
+  const screenOptions = (route, color) => {
+     let iconName
+     switch (route.name) {
+          case "restaurants-stack":
+            iconName = "compass-outline"
+            break;
+          case "search-stack":
+            iconName = "magnify"
+            break;
+          case "favourites-stack":
+            iconName = "heart-outline"
+            break;
+          case "top-restaurants-stack":
+            iconName = "star-outline"
+            break;
+          case "account-stack":
+            iconName = "home-outline"
+            break;
+     }
+     return(
+      <Icon
+          type="material-community"
+          name={iconName}
+          size={22}
+          color={color} 
+      />
+     )
+  }
+
   return (
     <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+            initialRouteName="restaurants-stack"
+            screenOptions={({route}) => ({
+                tabBarIcon: ({color}) => screenOptions(route, color),
+                tabBarActiveTintColor: "#452783",
+                tabBarInactiveTintColor: "#a17dc3"
+                
+            })}       
+        >
             <Tab.Screen
               name="restaurants-stack"
               component={RestaurantsStack}
